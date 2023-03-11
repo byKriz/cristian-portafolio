220d3ba7dc0643fc37e0761745f995ec072b2237
bykriz <cristianesquivelh@hotmail.com>
2023-03-11 18:21:52 -0400

    terminando la seccion Home

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "./Home.module.scss";
 import profileImg from "@assets/hero.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Home = () => {
   return (
     <section id="Home" className={styles.Home}>
-        <div className={styles.bannerContainer}>
-            <div className={styles.imgContainer}>
-                <img src={profileImg} alt="" />
-            </div>
-            <h1>Cristian Esquivel</h1>
+      <div className={styles.bannerContainer}>
+        <div className={styles.imgContainer}>
+          <img src={profileImg} alt="" />
         </div>
+        <h1>Cristian Esquivel</h1>
+        <h2>Frontend Developer - ReactJs Expert</h2>
+        <div className={styles.socialMedia}>
+          <a href="#"><FaGithub/></a>
+          <a href="#"><FaLinkedin/></a>
+        </div>
+      </div>
     </section>
   );
 };
