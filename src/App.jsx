6354abcd3254b1d5280d82b1004374fbd25dc8f7
bykriz <cristianesquivelh@hotmail.com>
2023-03-11 18:48:55 -0400

    agregando la sección aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { Header } from './components/Header/Header'
 import { Home } from './container/Home/Home'
+import { AboutMe } from './container/AboutMe/AboutMe'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Home />
+      <AboutMe />
     </div>
   )
 }
---
+++ src/container/AboutMe/AboutMe.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./AboutMe.module.scss";
+
+export const AboutMe = () => {
+  return (
+    <section id="about" className={styles.AboutMe}>
+      <div className={styles.aboutContainer}>
+        <h2>About Me</h2>
+        <p>
+          <span>Hi, i am Cristian Esquivel, </span>a frontend web developer with
+          a passion for creating beautiful and functional user interfaces. I
+          specialize in ReactJS, a powerful JavaScript library for building
+          dynamic and interactive web applications. I have experience in
+          developing responsive websites using HTML, CSS and JavaScript, as well
+          as working with RESTful APIs and popular frameworks like Bootstrap and
+          Tailwind. I also have knowledge of web accessibility, performance
+          optimization.
+        </p>
+        <p>
+          I'm always eager to learn new skills and technologies to improve my
+          craft as a frontend developer. I enjoy collaborating with other
+          developers and designers to create amazing web experiences for users.
+        </p>
+        <p>
+          If you are interested in working with me or just want to say hi, feel
+          free to contact me through email or LinkedIn. I'd love to hear from
+          you!
+        </p>
+      </div>
+    </section>
+  );
+};
