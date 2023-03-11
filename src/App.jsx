528a1c4de758808597420ba7034583be4d65076a
bykriz <cristianesquivelh@hotmail.com>
2023-03-11 17:38:11 -0400

    implementando Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Header } from './components/Header/Header'
+import { Home } from './container/Home/Home'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   )
 }
