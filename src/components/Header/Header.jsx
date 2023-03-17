558ec3b447219cb08f601adce52876c7e854ca6f
bykriz <cristianesquivelh@hotmail.com>
2023-03-17 12:21:04 -0400

    terminando el responsive del header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 export const Header = () => {
+  const [navMenu, setNavMenu] = useState(false);
+
+  const handleMenu = () => {
+    setNavMenu(!navMenu);
+  };
+
   return (
     <div className={styles.headerContainer}>
       <header>
         <div className={styles.logo}>
           <a href="#">Cristian Esquivel</a>
         </div>
-        <nav id="nav">
+        <nav id="nav" className={navMenu ? `${styles.responsive}` : ``}>
           <ul>
             <li>
-              <a href="#home">Home</a>
+              <a href="#home" onClick={handleMenu}>
+                Home
+              </a>
             </li>
             <li>
-              <a href="#aboutme">About Me</a>
+              <a href="#aboutme" onClick={handleMenu}>
+                About Me
+              </a>
             </li>
             <li>
-              <a href="#skills">Skills</a>
+              <a href="#skills" onClick={handleMenu}>
+                Skills
+              </a>
             </li>
             {/* <li>
               <a href="#curriculum">Curriculum</a>
             </li> */}
             <li>
-              <a href="#portfolio">Portafolio</a>
+              <a href="#portfolio" onClick={handleMenu}>
+                Portafolio
+              </a>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <a href="#contact" onClick={handleMenu}>
+                Contact
+              </a>
             </li>
           </ul>
         </nav>
-        <div className={styles.navResponsive}>
-          <FaBars />
+        <div className={styles.navResponsive} onClick={handleMenu}>
+          {!navMenu ? <FaBars /> : <AiOutlineClose />}
         </div>
       </header>
     </div>
